2bff9d8f8591d7857e740496182f43c48a31a13a
Kirill Mikhel <kirill.mikhel@gmail.com>
2022-03-12 20:26:10 +0100

    Change locale to en

--- src/localeContext.js
+++ src/localeContext.js
@@ -3,7 +3,7 @@ import { createContext, useContext, useState } from 'react'
 const localeContext = createContext()
 
 export const LocaleProvider = ({ children }) => {
-  const [locale] = useState('ru')
+  const [locale] = useState('en')
   return (
     <localeContext.Provider value={locale}>{children}</localeContext.Provider>
   )
