e6ff886f73aa1a04aec34d1c35b7b710a46d881d
Kirill Mikhel <kirill.mikhel@gmail.com>
2022-06-01 21:30:08 +0200

    Tweak the TopBar title

--- src/components/TopBar/TopBar.js
+++ src/components/TopBar/TopBar.js
@@ -2,29 +2,19 @@ import { AppBar, Toolbar, Typography, Box } from '@mui/material'
 import { LocaleSelector } from '../LocaleSelector/LocaleSelector'
 import logo from './logo192.png'
 
-export const TopBar = () => {
-  const title = ['Aidmap', 'Help for Ukrainian Refugees']
-
-  return (
-    <AppBar position="static">
-      <Toolbar>
-        <img src={logo} height={30} />
-        <Box sx={{ flexGrow: 1 }} ml={1} overflow="hidden">
-          {title.map((text) => (
-            <Typography
-              key={text}
-              component="h1"
-              variant="button"
-              display="block"
-              whiteSpace="nowrap"
-              sx={{ lineHeight: 1.3 }}
-            >
-              {text}
-            </Typography>
-          ))}
-        </Box>
-        <LocaleSelector />
-      </Toolbar>
-    </AppBar>
-  )
-}
+export const TopBar = () => (
+  <AppBar position="static">
+    <Toolbar>
+      <img src={logo} height={30} />
+      <Box sx={{ flexGrow: 1 }} ml={1} overflow="hidden">
+        <Typography component="h1" variant="button" sx={{ lineHeight: 1.3 }}>
+          Aidmap
+        </Typography>
+        <Typography component="h2" variant="caption" whiteSpace="nowrap" sx={{ lineHeight: 1.3 }}>
+          Help for Ukrainian Refugees
+        </Typography>
+      </Box>
+      <LocaleSelector />
+    </Toolbar>
+  </AppBar>
+)
