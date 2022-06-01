21ace7b06c6f90fcb5926837f831f9a345d27aea
Kirill Mikhel <kirill.mikhel@gmail.com>
2022-06-01 13:05:36 +0200

    Add branding

--- src/components/TopBar/TopBar.js
+++ src/components/TopBar/TopBar.js
@@ -1,19 +1,22 @@
 import { AppBar, Toolbar, Typography, Box } from '@mui/material'
 import { LocaleSelector } from '../LocaleSelector/LocaleSelector'
+import logo from './logo192.png'
 
 export const TopBar = () => {
-  const title = ['Ukraine Refugees', 'Help Map']
+  const title = ['Aidmap', 'Help for Ukrainian Refugees']
 
   return (
     <AppBar position="static">
       <Toolbar>
-        <Box sx={{ flexGrow: 1 }}>
+        <img src={logo} height={30} />
+        <Box sx={{ flexGrow: 1 }} ml={1} overflow="hidden">
           {title.map((text) => (
             <Typography
               key={text}
               component="h1"
               variant="button"
               display="block"
+              whiteSpace="nowrap"
               sx={{ lineHeight: 1.3 }}
             >
               {text}
